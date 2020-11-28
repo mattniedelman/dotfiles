dcfe04740a2d579082c02eb451c312b4715117bd
Matt Niedelman <mattniedelman@gmail.com>
2020-11-27 22:07:30 -0600

    setup zsh

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -4,18 +4,69 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "mattniedelman";
-  home.homeDirectory = "/home/mattniedelman";
+  home = {
+    username = "mattniedelman";
+    homeDirectory = "/home/mattniedelman";
+    stateVersion = "21.03";
+    sessionVariables = {
+      LANG = "en_US.UTF-8";
+      LANGUAGE = "en_US.UTF-8";
+    };
+    packages = with pkgs; [
+      htop
+      tree
+      wget
+      unzip
+      gnumake
+      powerline-fonts
+      glibcLocales
+      zplug
+    ];
+  };
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "21.03";
+
+  programs.git = {
+    enable = true;
+    userName  = "mattniedelman";
+    userEmail = "mattniedelman@gmail.com";
+    extraConfig = {
+      core.editor = "nvim";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    };
+
+  programs.bat = {
+    enable = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+  };
+  
+  programs.go = {
+    enable = true;
+  };
+
+  programs.jq = {
+    enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    zplug = {
+      enable = true;
+    };
+    initExtraBeforeCompInit = ''
+      #!/bin/env zsh
+      source ${pkgs.zplug}/init.zsh
+      ${builtins.readFile ./.zplugrc}
+  '';
+  };
 }
