92745141ad3c6e0549f12a1a7a12ac86f3ad12c1
Matt Niedelman <matt.niedelman@kaizenanalytix.com>
2022-06-03 17:24:31 -0500

    Update private_dot_config/nvim/readonly_init.vim Add run_once_install_packages.sh

---
+++ run_once_install_packages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+npm install -g \
+    @angular/language-server \
+    @ansible/ansible-language-server \
+    bash-language-server \
+    vscode-langservers-extracted \
+    cssmodules-language-server \
+    dockerls \
+    spectral \
+    typescript-language-server \
+    vim-language-server \
+    yarn
+
+yarn global add \
+    diagnostic-languageserver \
+    yaml-language-server
+
+dotnet tool install --global csharp-ls
+
+pip install 'python-lsp-server[all]'
+
+cargo install --locked taplo-cli
