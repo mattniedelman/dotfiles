47b92b1308f30c8f89113f5fb6aad3bc21809323
Matt Niedelman <mattniedelman@gmail.com>
2022-10-21 10:59:01 -0500

    chezmoi updates

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,13 +1,17 @@
+
+source "$HOME/.asdf/asdf.fish"
 if status is-interactive
-    source "$HOME/.asdf/asdf.fish"
     # Commands to run in interactive sessions can go here
     fish_add_path "$HOME/bin"
 
 end
 
-set EDITOR nvim
+set -Ux EDITOR nvim
+set -Ux fish_greeting ""
 
-alias cdr='cd $(git rev-parse --show-toplevel)'
+alias cdr='cd (git rev-parse --show-toplevel)'
 alias k='kubectl'
 alias tf='terraform'
+
+fzf_configure_bindings --directory=\ct
 
