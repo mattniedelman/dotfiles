239f710d5a320eb0151cf4afe38b76c930e88ed7
Matt Niedelman <matt.niedelman@imprivata.com>
2022-11-22 09:32:27 -0600

    update fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,15 +3,19 @@ source "$HOME/.asdf/asdf.fish"
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path "$HOME/bin"
-
+    fish_add_path "$GOPATH/bin"
 end
 
 set -Ux EDITOR nvim
 set -Ux fish_greeting ""
+set -Ux SSH_AUTH_SOCK ~/.1password/agent.sock
+set -Ux AWS_DEFAULT_PROFILE 439323037767_AI-Developer
+set -Ux DIRENV_LOG_FORMAT ""
 
 alias cdr='cd (git rev-parse --show-toplevel)'
 alias k='kubectl'
 alias tf='terraform'
+alias icat="kitty +kitten icat"
 
 fzf_configure_bindings --directory=\ct
 
