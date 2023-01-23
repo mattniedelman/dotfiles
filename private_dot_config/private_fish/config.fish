284688250c6ee1cbbcdb1fd75b8423c43c3e1c0b
Matt Niedelman <mattniedelman@gmail.com>
2023-01-23 11:21:04 -0600

    fish TERM config and nvim alias

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -11,11 +11,13 @@ set -Ux fish_greeting ""
 set -Ux SSH_AUTH_SOCK ~/.1password/agent.sock
 set -Ux AWS_DEFAULT_PROFILE 439323037767_AI-Developer
 set -Ux DIRENV_LOG_FORMAT ""
+set -Ux TERM xterm-color
 
 alias cdr='cd (git rev-parse --show-toplevel)'
 alias k='kubectl'
 alias tf='terraform'
 alias icat="kitty +kitten icat"
+alias nvim=nvimvenv
 
 fzf_configure_bindings --directory=\ct
 
