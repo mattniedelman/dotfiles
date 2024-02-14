ba71bf82a3caa3c1d71eaa27a0a49b0e9c003de7
Matt Niedelman <mattniedelman@gmail.com>
2024-02-14 12:27:06 -0600

    updates

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -2,6 +2,13 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters = {
+        sqlfmt = {
+          command = "sqlfmt",
+          args = { "-" },
+        },
+      },
+
       formatters_by_ft = {
         lua = { "stylua" },
         fish = { "fish_indent" },
@@ -10,7 +17,10 @@ return {
         json = { "jq" },
         go = { "gofumpt", "goimports" },
         markdown = { "markdownlint", "mdslw" },
-        yaml = { "yamlfix" },
+        yaml = { "yamlfix", "yamlfmt" },
+        sql = { "sqlfmt" },
+        dbt = { "sqlfmt" },
+        ["*"] = { "trim_whitespace", "trim_newlines", "typos" },
       },
     },
   },
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,6 +4,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path "$HOME/bin"
     fish_add_path "$GOPATH/bin"
+    fish_add_path "$HOME/.krew/bin"
 end
 
 set -Ux EDITOR nvim
