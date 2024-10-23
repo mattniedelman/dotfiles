52f389d25832e5b8c8cc699c3154887aa1278415
Matt Niedelman <mattniedelman@gmail.com>
2024-10-23 12:51:48 -0500

    checkpoint

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -1,55 +1,40 @@
 return {
   {
+    "zbirenbaum/copilot.lua",
+    opts = function(_, opts)
+      opts.suggestion = {
+        enabled = false,
+        auto_trigger = true,
+      }
+    end,
+  },
+  {
+
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
-      local has_words_before = function()
-        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-          return false
-        end
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-      end
+      local cmp = require "cmp"
 
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      -- Disable completion in comments
-      cmp.setup({
-        enabled = function()
-          local context = require("cmp.config.context")
-          if vim.api.nvim_get_mode().mode == "c" then
-            return true
-          else
-            return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
-          end
-        end,
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+        priority = 10000,
       })
 
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        ["<S-CR>"] = cmp.mapping.confirm({ select = false }),
-        ["<C-f>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = vim.schedule_wrap(function(fallback)
-          if cmp.visible() and has_words_before() then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-          else
-            fallback()
-          end
-        end),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
       opts.preselect = cmp.PreselectMode.None
       opts.completion = {
         completeopt = "noselect",
       }
+      cmp.setup {
+        mapping = {
+          ["<Tab>"] = function(fallback) fallback() end,
+        },
+      }
+
+      require("astrocore").extend_tbl(opts, {
+        mapping = {
+          ["<C-f>"] = cmp.mapping.confirm { select = true },
+        },
+      })
     end,
   },
 }
