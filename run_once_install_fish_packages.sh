267b3176695747a23de09e224b002f6499ef5812
Matt Niedelman <mattniedelman@gmail.com>
2025-11-10 11:56:18 -0600

    remove setup scripts for now

--- run_once_install_fish_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/fish
-fisher install \
-	jorgebucaran/fisher \
-	IlanCosman/tide@v5 \
-	PatrickF1/fzf.fish \
-	gazorby/fish-abbreviation-tips \
-	fischerling/plugin-wd \
-	acomagu/fish-async-prompt
