208ae1ff9d16c6095021ce54bbad114294c46f09
Matt Niedelman <mattniedelman@gmail.com>
2026-02-06 09:47:50 -0600

    get hooks going again

--- dot_augment/hooks/executable_ascii_check.sh
+++ dot_augment/hooks/executable_ascii_check.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S uv run --quiet --directory /home/mattniedelman/.augment/hooks
+#!/usr/bin/env -S uv run --quiet --script --directory /home/mattniedelman/.augment/hooks
 """
 PostToolUse hook: Auto-fix Unicode lookalike characters in file changes.
 
